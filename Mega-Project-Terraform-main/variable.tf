c2aaa511f4e4d00572aa5e34ba32f5b8abe943b6
Kama singh <kamlesh7327@gmail.com>
2025-03-21 19:23:15 +0530

    key update

--- Mega-Project-Terraform-main/variable.tf
+++ Mega-Project-Terraform-main/variable.tf
@@ -1,6 +1,5 @@
 variable "ssh_key_name" {
   description = "The name of the SSH key pair to use for instances"
   type        = string
-  default     = "/home/ubuntu/.ssh/id_ed25519.pub"
-
+  default     = "DevOps-Shack"
 }
